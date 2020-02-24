f4cb22b124b9d0e054ab7ca4a2b7c88aa8260bbc
Veronika Baraniak <veronika.baraniak@eleks.com>
2020-02-24 18:52:45 +0200

    add services component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ServicesComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: HomeComponent}])
